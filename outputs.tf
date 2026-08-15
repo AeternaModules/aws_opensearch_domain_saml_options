c2de8dfedbbb92e02e8a22627df54e486539190c
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:45:52 +0100

    Update to schema version v6.58.0

--- outputs.tf
+++ outputs.tf
@@ -12,7 +12,7 @@ output "opensearch_domain_saml_options_region" {
 }
 output "opensearch_domain_saml_options_saml_options" {
   description = "Map of saml_options values across all opensearch_domain_saml_options, keyed the same as var.opensearch_domain_saml_options"
-  value       = { for k, v in aws_opensearch_domain_saml_options.opensearch_domain_saml_options : k => v.saml_options if v.saml_options != null && length(v.saml_options) > 0 }
+  value       = { for k, v in aws_opensearch_domain_saml_options.opensearch_domain_saml_options : k => one(v.saml_options) if v.saml_options != null && length(v.saml_options) > 0 }
   sensitive   = true
 }
 
